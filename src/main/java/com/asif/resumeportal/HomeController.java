9c13c7a4bced505a31c496af156b17ffa82c9463
asifjoardar <mrsparrow04@gmail.com>
2023-06-15 18:00:26 +0600

    added update info logics

--- src/main/java/com/asif/resumeportal/HomeController.java
+++ src/main/java/com/asif/resumeportal/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -63,10 +64,15 @@ public class HomeController {
     }
 
     @PostMapping("/edit")
-    public String postEdit(Model model, Principal principal){
-        String userId = principal.getName();
-        // save
-        return "redirect:/view/" + userId;
+    public String postEdit(Model model, Principal principal, @ModelAttribute UserProfile userProfile){
+        String userName = principal.getName();
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByUserName(userName);
+        userProfileOptional.orElseThrow(() -> new RuntimeException("not found: " + userName));
+        UserProfile savedUserProfile = userProfileOptional.get();
+        userProfile.setId(savedUserProfile.getId());
+        userProfile.setUserName(userName);
+        userProfileRepository.save(userProfile);
+        return "redirect:/view/" + userName;
     }
 
     @GetMapping("/view/{userId}")
@@ -80,6 +86,5 @@ public class HomeController {
         model.addAttribute("userProfile", userProfile);
 
         return "profile-templates/" + userProfile.getTheme() + "/index";
-        //return "profile";
     }
 }
